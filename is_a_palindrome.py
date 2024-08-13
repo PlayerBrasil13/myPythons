bc1955d959d2c3de29101e3a5127697aab94ba9e
Iago Ribeiro Souto <iagoplayer13@gmail.com>
2024-08-13 09:07:32 -0300

    Added is_a_palindrome

---
+++ is_a_palindrome.py
@@ -0,0 +1,15 @@
+text = input("Enter your text to check if it is a palindrome: ")
+text = text.lower() # Remove uppercase letters
+aux_text = ""
+global text2, text3
+text2, text3 = "", ""
+for ch in text: # Remove whitespaces and reverse the order of the string
+    
+    if ch.isalpha():
+        text2 += ch
+        text3 = ch + text3
+
+if text2 == text3:
+    print("It's a palindrome")
+else:
+    print("It's not a palindrome")
